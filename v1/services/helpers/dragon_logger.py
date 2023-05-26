1e9ecb6719cb0b2e6ea3db0b0f3be4f6eb1e3076
itunisoftgroup <maxalphaengineer@gmail.com>
2023-05-26 12:23:46 +0500

    Added: Decorator template

--- v1/services/helpers/dragon_logger.py
+++ v1/services/helpers/dragon_logger.py
@@ -15,3 +15,34 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with "Django JsonRPC Server Template".  If not, see <http://www.gnu.org/licenses/>.
+
+# defining a decorator
+def hello_decorator(func):
+    # inner1 is a Wrapper function in
+    # which the argument is called
+
+    # inner function can access the outer local
+    # functions like in this case "func"
+    def inner1():
+        print("Hello, this is before function execution")
+
+        # calling the actual function now
+        # inside the wrapper function.
+        func()
+
+        print("This is after function execution")
+
+    return inner1
+
+
+# defining a function, to be called inside wrapper
+def function_to_be_used():
+    print("This is inside the function !!")
+
+
+# passing 'function_to_be_used' inside the
+# decorator to control its behaviour
+function_to_be_used = hello_decorator(function_to_be_used)
+
+# calling the function
+function_to_be_used()
